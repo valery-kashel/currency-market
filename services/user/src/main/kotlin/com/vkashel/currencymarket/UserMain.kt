dc6c31f78323e70df68705a6c7ac57dc4745deff
Valery <valery.kashel@gmail.com>
2022-08-28 18:32:56 +0200

    Removed HelloUserRoute

--- services/user/src/main/kotlin/com/vkashel/currencymarket/UserMain.kt
+++ services/user/src/main/kotlin/com/vkashel/currencymarket/UserMain.kt
@@ -2,7 +2,6 @@ package com.vkashel.currencymarket
 
 import com.vkashel.currencymarket.endpoints.CreateUserRoute
 import com.vkashel.currencymarket.endpoints.GetUserRoute
-import com.vkashel.currencymarket.endpoints.HelloUserRoute
 import com.vkashel.currencymarket.repositories.InMemoryUserRepository
 import com.vkashel.currencymarket.server.RestServer
 import com.vkashel.currencymarket.services.UserService
@@ -11,7 +10,6 @@ fun main() {
     val userRepository = InMemoryUserRepository()
     val userService = UserService(userRepository)
     val server = RestServer(
-        HelloUserRoute(),
         CreateUserRoute(userService),
         GetUserRoute(userService)
     )
